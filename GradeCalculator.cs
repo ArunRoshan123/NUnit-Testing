3ea9d7a534f1b176e23dcd9a30e864e0545b258e
A.R. Arun Roshan <arunraksha234@gmail.com>
2024-02-13 15:05:24 +0530

    [Arun] Add: Implemented nUnit Testing

---
+++ GradeCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting
+{
+    public class GradeCalculator
+    {
+        public string GetGradeByPercentage(int percent)
+        {
+            if (percent >= 90 && percent <= 100)
+                return "O";
+            else if (percent >= 80 && percent <= 89)
+                return "A+";
+            else if (percent >= 70 && percent <= 79)
+                return "A";
+            else if (percent >= 60 && percent <= 69)
+                return "B+";
+            else if (percent >= 50 && percent <= 59)
+                return "B";
+            else if (percent <= 49)
+                return "F";
+            else 
+                return "Invalid";
+        }
+    }
+}
