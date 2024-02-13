aeb7a03e73eff17a786d28dc9ebf9d32a823c126
A.R. Arun Roshan <arunraksha234@gmail.com>
2024-02-13 15:03:49 +0530

    [Arun] Add: Implemented nUnit Testing

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeCalculator g1 = new GradeCalculator();
+
+            Console.WriteLine("Enter percentage ");
+            var percentage = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Student grade : {g1.GetGradeByPercentage(percentage)}");
+
+        }
+    }
+}
